4530ae0d6d6ff49f57b941d4c2eef0607ba591b1
jvrsgsty <jvrsgsty@users.noreply.github.com>
2015-01-30 01:28:22 +0100

    mod: added support for XMLUnit configuration
    mod: makefile to support new files
    mod: NOTES explaining the changes in more detail
    mod: J2WsdlRegrTest.java changed the way the tests are done to support
    overriding the DifferenceListener. Added two new tests: one to test the
    configuration of XMLUnit, one to test a new interface: ManyMethods
    add: ManyMethods.java interface
    add: myDifferenceListener.java custom DifferenceListener to ignore
    certain attributes on XML elements
    
    Aftermath:
    The no extension and filename tests fail as the should.
    The NoMethod tests passes (as it should, ignoring commentaries and
    certain attributes)
    The xmlUnitConfigurationPassed passes
    The ManyMethods test fails because some xml elements are missing on the
    output of the new version, namely Request/Response elements on the
    method description

--- aufg5/src/J2WsdlRegrTest.java
+++ aufg5/src/J2WsdlRegrTest.java
@@ -1,37 +1,41 @@
-import static org.junit.Assert.*;
-import static org.custommonkey.xmlunit.XMLAssert.*;
+import org.junit.Assert;
+import org.custommonkey.xmlunit.XMLAssert;
 
 import org.junit.Test;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.custommonkey.xmlunit.XMLUnit;
+import org.custommonkey.xmlunit.Diff;
 
 import java.io.File;
 
 public class J2WsdlRegrTest
 {
 	@BeforeClass
-	public static void prepareWsdlDirs ()
-	{
+	public static void setUp(){
 		RegressionHelpers.prepareWsdlDir();
+  	XMLUnit.setIgnoreWhitespace(true); 
+  	XMLUnit.setNormalizeWhitespace(true); 
+  	XMLUnit.setIgnoreComments(true);
 	}
 
-	//@Test
-	//public void filesHaveNoExtName ()
-		//throws java.lang.InterruptedException, java.io.IOException
-	//{
-		//final String Class = "NoMethod";
-		//final String FileName = "xml";
-		//RegressionHelpers.convert2WsdlWithLib(Class, "old", FileName);
-		//RegressionHelpers.convert2WsdlWithLib(Class, "new", FileName);
-		//File oldWsdlDir = RegressionHelpers.getWsdlDir(Class, "old");
-		//File newWsdlDir = RegressionHelpers.getWsdlDir(Class, "new");
-		//String newFileName = oldWsdlDir.list()[0];
-		//String oldFileName = newWsdlDir.list()[0];
-//
-		//assertEquals("Generate files with no extension",
-			//oldFileName, newFileName);
-	//}
+	// @Test
+	// public void filesHaveNoExtName ()
+	// 	throws java.lang.InterruptedException, java.io.IOException
+	// {
+	// 	final String Class = "NoMethod";
+	// 	final String FileName = String.format("%s", Class);
+	// 	RegressionHelpers.convert2WsdlWithLib(Class, "old", FileName);
+	// 	RegressionHelpers.convert2WsdlWithLib(Class, "new", FileName);
+	// 	File oldWsdlDir = RegressionHelpers.getWsdlDir(Class, "old");
+	// 	File newWsdlDir = RegressionHelpers.getWsdlDir(Class, "new");
+	// 	String newFileName = oldWsdlDir.list()[0];
+	// 	String oldFileName = newWsdlDir.list()[0];
+
+	// 	Assert.assertEquals("The file names are different",
+	// 		oldFileName, newFileName);
+	// }
 
 	@Test
 	public void filesHaveSameName ()
@@ -46,12 +50,12 @@ public class J2WsdlRegrTest
 		String newFileName = oldWsdlDir.list()[0];
 		String oldFileName = newWsdlDir.list()[0];
 
-		assertEquals("Generated files have the same name",
+		Assert.assertEquals("The file names are different",
 			oldFileName, newFileName);
 	}
 
 	@Test
-	public void sameNoMethodXML ()
+	public void xmlEqualNoMethod ()
 		throws java.io.IOException, org.xml.sax.SAXException
 	{
 		File xmlFile;
@@ -63,6 +67,75 @@ public class J2WsdlRegrTest
 		xmlFile = RegressionHelpers.getWsdlDir("NoMethod", "new").listFiles()[0];
 		newXML = RegressionHelpers.loadXmlString(xmlFile);
 
-		assertXMLEqual("NoMethod", oldXML, newXML);
+		Diff diff = new Diff(oldXML, newXML);
+		diff.overrideDifferenceListener(
+			new myDifferenceListener());
+		XMLAssert.assertTrue("NoMethod", diff.similar());
+		// XMLAssert.assertXMLEqual("NoMethod", oldXML, newXML);
+	}
+
+	@Test
+	public void xmlEqualManyMethods ()
+		throws java.io.IOException, org.xml.sax.SAXException, java.lang.InterruptedException
+	{
+		File xmlFile;
+		String oldXML;
+		String newXML;
+		final String Class = "ManyMethods";
+		final String FileName = String.format("%s.xml", Class);
+		RegressionHelpers.convert2WsdlWithLib(Class, "old", FileName);
+		RegressionHelpers.convert2WsdlWithLib(Class, "new", FileName);
+		xmlFile = RegressionHelpers.getWsdlDir("ManyMethods", "old").listFiles()[0];
+		oldXML = RegressionHelpers.loadXmlString(xmlFile);
+		xmlFile = RegressionHelpers.getWsdlDir("ManyMethods", "new").listFiles()[0];
+		newXML = RegressionHelpers.loadXmlString(xmlFile);
+
+		Diff diff = new Diff(oldXML, newXML);
+		diff.overrideDifferenceListener(
+			new myDifferenceListener());
+		XMLAssert.assertTrue("ManyMethods", diff.similar());
+	}
+	
+
+	@Test
+	public void xmlUnitConfigurationPassed()
+		throws java.io.IOException, org.xml.sax.SAXException
+	{
+		String control = "<?xml version='1.0' encoding='UTF-8'?> \n" +
+									"		<books> \n" +
+									"<!-- comment 1-->" +
+									"		    <book> \n" +
+									"		        <name>Angels &amp; Demons</name> \n" +
+									"		        <isbn>9971-5-0210-0</isbn> \n" +
+									"		        <author transport='http'>Dan Brown</author> \n" +
+									"		        <category></category> \n" +
+									"		    </book> \n" +
+									"		    <book> \n" +
+									"		        <name>You can         win</name> \n" +
+									"		        <isbn>9971-5-0222-0</isbn> \n" +
+									"		        <author>Shiv Khera</author> \n" +
+									"		    </book> \n" +
+									"		</books>";
+
+		String test = "<?xml version='1.0' encoding='UTF-8'?> \n" +
+									"		<books> \n" +
+									"		    <book> \n" +
+									"		        <name>Angels &amp; Demons</name> \n" +
+									"		        <isbn>9971-5-0210-0</isbn> \n" +
+									"		        <author transport='http11'>Dan Brown</author> \n" +
+									"		        <category></category> \n" +
+									"		    </book> \n" +
+									"<!-- comment 2               -->" +
+									"		    <book> \n" +
+									"		        <name>You can win</name> \n" +
+									"		        <isbn>9971-5-0222-0</isbn> \n" +
+									"		        <author>Shiv Khera</author> \n" +
+									"		    </book> \n" +
+									"		</books>";
+		Diff diff = new Diff(control, test);
+		diff.overrideDifferenceListener(
+			new myDifferenceListener());
+		XMLAssert.assertTrue("Files are not the same", diff.similar());
+		//assertXMLEqual("Files are not the same", control, test);
 	}
 }
